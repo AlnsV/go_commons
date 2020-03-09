127277ea044d9a957687aee08c4e19f3c7735c1b
alnsv <alonso6230@gmail.com>
2020-03-09 13:14:40 -0400

    initial commit

---
+++ alert.go
@@ -0,0 +1,64 @@
+package commons
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Alert interface {
+	Send(subject, message, alertTopic string) error
+}
+
+// AlertHandler manages the timeout for generating an alert
+type AlertHandler struct {
+	activationTimes int
+	requestUrl      string
+	lastSubject     string
+	lastMessage     string
+	lastAlert       time.Time
+	timeout         time.Duration
+}
+
+func NewAlertHandler(timeout time.Duration, url string) *AlertHandler {
+	return &AlertHandler{
+		requestUrl:      url,
+		activationTimes: 0,
+		timeout:         timeout,
+	}
+}
+
+// AlertHandler_Send this method helps into sending an alert email
+func (a *AlertHandler) Send(subject, message, alertTopic string) error {
+	// Verifies if the alert activated too recently
+	if a.activationTimes != 0 && time.Now().UTC().Sub(a.lastAlert) < a.timeout {
+		a.activationTimes += 1
+		return nil
+	}
+
+	a.activationTimes += 1
+	a.lastAlert = time.Now().UTC()
+	a.lastMessage = message
+	a.lastSubject = subject
+
+	// Formatting the request body with the relevant info for our request
+	reqBody, _ := json.Marshal(map[string]string{
+		"subject": subject,
+		"mailBody": fmt.Sprintf(
+			"%s\nActivated: %d times.\nTimestamp:%s",
+			message,
+			a.activationTimes,
+			a.lastAlert.Format(time.RFC1123),
+		),
+		"topic": fmt.Sprintf("arn:aws:sns:us-east-2:406163507813:%s", alertTopic),
+	})
+
+	// Execution of the post requests with the follow URL
+	_, err := http.Post(a.requestUrl, "application/json", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return fmt.Errorf("failure sending Post request for sending email alert, err: %s", err)
+	}
+	return nil
+}
---
+++ alert_test.go
@@ -0,0 +1,43 @@
+package commons
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNewAlertHandler(t *testing.T) {
+	t.Parallel()
+	_ = NewAlertHandler(5, "https://36l9op4nsg.execute-api.us-east-2.amazonaws.com/production/")
+}
+
+func TestAlertHandler_Send(t *testing.T) {
+	t.Parallel()
+	alert := NewAlertHandler(3*time.Second, "https://36l9op4nsg.execute-api.us-east-2.amazonaws.com/production/")
+	time.Sleep(5 * time.Second)
+	err := alert.Send(
+		"Test",
+		"Running test on preprocessor",
+		"testAlert",
+	)
+	if err != nil {
+		t.Errorf("Failure sending email: err %s", err)
+	}
+	err = alert.Send(
+		"Test2",
+		"holhsdfsdfjk",
+		"testAlert",
+	)
+	if err != nil {
+		t.Errorf("Failure sending email: err %s", err)
+	}
+	alertTwo := NewAlertHandler(3*time.Second, "https://36l9op4nasdafsil872i8734hasi4ba8ys.com/asdhaskjdh/")
+	time.Sleep(5 * time.Second)
+	errTwo := alertTwo.Send(
+		"Test",
+		"Running test on preprocessor",
+		"testAlert",
+	)
+	if errTwo == nil {
+		t.Error("this host shouldn't be found")
+	}
+}
